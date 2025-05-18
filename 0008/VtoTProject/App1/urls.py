c1753361b436c60b0dab9f743308313ebdd5e8f5
jvrkrishna <jvrkrishnaa@gmail.com>
2025-05-18 21:33:37 +0530

    lk

--- 0008/VtoTProject/App1/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('addA/', views.addAdmission),
-    path('admissionR/',views.admissionReport),
-]
--- 0008/VtoTProject/App1/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def sample_view(request):
-    return render(request, 'App1/sample.html')
