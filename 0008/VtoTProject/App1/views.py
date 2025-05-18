135c0cc66d31c78cec7c5901ee36e014147cc819
jvrkrishna <jvrkrishnaa@gmail.com>
2025-05-18 21:30:41 +0530

    k

--- 0008/VtoTProject/App1/views.py
+++ 0008/VtoTProject/App1/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def addAdmission(request):
-    return render(request, 'App1/add-admission.html')
-
-def admissionReport(request):
-    return render(request, 'App1/admission-report.html')
+def sample_view(request):
+    return render(request, 'App1/sample.html')
