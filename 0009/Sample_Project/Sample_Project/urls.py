f6f59f80bedcc5852a491916d2ab7c26933fa2bc
jvrkrishna <jvrkrishnaa@gmail.com>
2025-05-23 14:03:46 +0530

    done

--- 0009/Sample_Project/Sample_Project/urls.py
+++ 0009/Sample_Project/Sample_Project/urls.py
@@ -20,3 +20,9 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.conf.urls.static import static 
+from django.conf import settings
+if settings.DEBUG==False:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
