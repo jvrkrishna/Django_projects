39e4783dbbc38057e048d1e12b4888664710c843
jvrkrishna <jvrkrishnaa@gmail.com>
2025-05-27 10:13:31 +0530

    k

--- 0009/Sample_Project/Sample_Project/urls.py
+++ 0009/Sample_Project/Sample_Project/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home,about,contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='home'),
+    path('about/',about,name='about'),
+    path('contact/',contact,name='contact'),
 ]
 
 from django.conf.urls.static import static 
