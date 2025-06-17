0f853e2fc2832f92f611c1463d208a8da66f4302
jvrkrishna <jvrkrishnaa@gmail.com>
2025-06-17 23:48:45 +0530

    done

--- 0009/Sample_Project/Sample_Project/views.py
+++ 0009/Sample_Project/Sample_Project/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,HttpResponse
 
 def home(request):
-    return render(request,'index.html')
+    name='Rama Krishna'
+    friends=["Ram","Hari","Chanti"]
+    return render(request,'index.html',{'name':name,'friends':friends})
 
 def about(request):
     return render(request,'about.html')
