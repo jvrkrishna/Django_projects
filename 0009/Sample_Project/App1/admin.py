540b41b8c431d0c976347fb20048824922be6c9a
jvrkrishna <jvrkrishnaa@gmail.com>
2025-06-20 22:21:49 +0530

    done

--- 0009/Sample_Project/App1/admin.py
+++ 0009/Sample_Project/App1/admin.py
@@ -4,4 +4,5 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display=('id','title','created_at')
     list_display_links=('title',)
+    list_filter=('created_at','is_published',) #Add to get filters
 admin.site.register(Post,PostAdmin)
