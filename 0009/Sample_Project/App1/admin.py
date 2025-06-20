7a023260a3e37398c2dd8907b38299e59aa66e2c
jvrkrishna <jvrkrishnaa@gmail.com>
2025-06-20 19:12:52 +0530

    Done

--- 0009/Sample_Project/App1/admin.py
+++ 0009/Sample_Project/App1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display=('id','title','created_at')
+    list_display_links=('title',)
+admin.site.register(Post,PostAdmin)
